df1cae1d696925a519e978c0dead6d70a5d76195
sandyasekar <sandya.sekar@ui.ac.id>
2017-07-30 20:29:23 +0700

    service fixed

--- backend/resources/views/pages/services.blade.php
+++ backend/resources/views/pages/services.blade.php
@@ -18,35 +18,42 @@
       <h4 class="center">Service List</h4>
      <ul class="collapsible popout" data-collapsible="accordion">
          <li>
-            <div class="collapsible-header"><i class="material-icons">filter_drama</i>Chemicals Supply Management</div>
-            <div class="collapsible-body"><p>This is first section.</p></div>
-         </li>
-         <li>
-            <div class="collapsible-header"><i class="material-icons">place</i>Cost Efficiency</div>
+            <div class="collapsible-header"><img src="images/flasks.png" style="width: 30px;height: 30px; margin-top: 10px;" /><span class="pull-right">Chemicals Supply Management</span>               
+            </div>
             <div class="collapsible-body"><p>This is second section.</p></div>
          </li>
          <li>
-            <div class="collapsible-header"><i class="material-icons">whatshot</i>Delivery Management</div>
-            <div class="collapsible-body"><p>This is third section.</p></div>
-         </li>
-         <li>
-            <div class="collapsible-header"><i class="material-icons">filter_drama</i>Stock Management</div>
-            <div class="collapsible-body"><p>This is first section.</p></div>
-         </li>
-         <li>
-            <div class="collapsible-header"><i class="material-icons">place</i>Distribution Management</div>
+            <div class="collapsible-header"><img src="images/budget.png" style="width: 30px;height: 30px; margin-top: 10px;" /><span class="pull-right">Chemicals Supply Management</span>               
+            </div>
             <div class="collapsible-body"><p>This is second section.</p></div>
          </li>
          <li>
-            <div class="collapsible-header"><i class="material-icons">whatshot</i>Lab and Sample Management</div>
+            <!-- <div class="collapsible-header"><i class="material-icons">filter_drama</i>Chemicals Supply Management</div> -->
+            <div class="collapsible-header"><img src="images/delivery-truck.png" style="width: 30px;height: 30px; margin-top: 10px;" /><span class="pull-right">Delivery Management</span></div>
             <div class="collapsible-body"><p>This is third section.</p></div>
          </li>
          <li>
-            <div class="collapsible-header"><i class="material-icons">whatshot</i>HSE Management</div>
+            <!-- <div class="collapsible-header"><i class="material-icons">filter_drama</i>Chemicals Supply Management</div> -->
+            <div class="collapsible-header"><img src="images/warehouse.png" style="width: 30px;height: 30px; margin-top: 10px;" /><span class="pull-right">Stock Management</span></div>
             <div class="collapsible-body"><p>This is third section.</p></div>
          </li>
          <li>
-            <div class="collapsible-header"><i class="material-icons">whatshot</i>Quality Management</div>
+            <!-- <div class="collapsible-header"><i class="material-icons">filter_drama</i>Chemicals Supply Management</div> -->
+            <div class="collapsible-header"><img src="images/cargo.png" style="width: 30px;height: 30px; margin-top: 10px;" /><span class="pull-right">Distribution Management</span></div>
+            <div class="collapsible-body"><p>This is third section.</p></div>
+         </li>
+         <li>
+            <!-- <div class="collapsible-header"><i class="material-icons">filter_drama</i>Chemicals Supply Management</div> -->
+            <div class="collapsible-header"><img src="images/laboratory.png" style="width: 30px;height: 30px; margin-top: 10px;" /><span class="pull-right">Lab and Sample Management</span></div>
+            <div class="collapsible-body"><p>This is third section.</p></div>
+         </li>
+         <li><!-- <div class="collapsible-header"><i class="material-icons">filter_drama</i>Chemicals Supply Management</div> -->
+            <div class="collapsible-header"><img src="images/charity.png" style="width: 30px;height: 30px; margin-top: 10px;" /><span class="pull-right">HSE Management</span></div>
+            <div class="collapsible-body"><p>This is third section.</p></div>
+         </li>
+         <li>
+            <!-- <div class="collapsible-header"><i class="material-icons">filter_drama</i>Chemicals Supply Management</div> -->
+            <div class="collapsible-header"><img src="images/5-stars-sign.png" style="width: 30px;height: 30px; margin-top: 10px;" /><span class="pull-right">Lab and Sample Management</span></div>
             <div class="collapsible-body"><p>This is third section.</p></div>
          </li>
 
