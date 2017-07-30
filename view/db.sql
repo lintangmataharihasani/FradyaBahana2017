686d3284255c445a3a6cf92dcb1fb9f4a9591183
Lintang Matahari <sternundsonne@gmail.com>
2017-07-30 20:19:26 +0700

    add new sql fixed

--- view/db.sql
+++ view/db.sql
@@ -97,6 +97,8 @@ INSERT INTO service (nama, deskripsi) VALUES ("Lab and Sample Management", "Lore
 INSERT INTO service (nama, deskripsi) VALUES ("HSE Management", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
 	Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac.");
 
-
-
-
+INSERT INTO konten (nama_konten, konten) VALUES ("about_us", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae");
+INSERT INTO konten (nama_konten, konten) VALUES ("footer_bio", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae");
+INSERT INTO konten (nama_konten, konten) VALUES ("tagline_home", "A reliable partner in total chemicals management");
+INSERT INTO konten (nama_konten, konten) VALUES ("tagline_product", "We provide best chemical products for your industry needs");
+INSERT INTO konten (nama_konten, konten) VALUES ("tagline_service", "We provide high quality and personalized services your industry needs");
