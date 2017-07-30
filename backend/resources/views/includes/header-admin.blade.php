d568a88041fc6f52cad66c63176272de0e4d6600
hafizh19adnan <hafizh19adnan@gmail.com>
2017-07-30 20:42:06 +0700

    fix

--- backend/resources/views/includes/header-admin.blade.php
+++ backend/resources/views/includes/header-admin.blade.php
@@ -7,12 +7,12 @@
     <ul class="right hide-on-med-and-down">
       
       <li><a href="#" class="white-text">Home</a></li>
-      <li><a href="service.html" class="white-text">Logout</a></li>
+      <li><a href="#" class="white-text">Logout</a></li>
     </ul>
 
     <ul id="nav-mobile" class="side-nav">
       <li><a href="#" class="white-text">Home</a></li>
-      <li><a href="service.html" class="white-text">Logout</a></li>
+      <li><a href="#" class="white-text">Logout</a></li>
     </ul>
     <a href="#" data-activates="nav-mobile" class="button-collapse white-text"><i class="material-icons">menu</i></a>
   </div>
