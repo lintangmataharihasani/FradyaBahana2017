7718c58357a09a2d5543374effcf461fec76fb64
hafizh19adnan <hafizh19adnan@gmail.com>
2017-08-08 14:03:15 +0700

    Collapsible fix cuk

--- backend/resources/views/includes/footer.blade.php
+++ backend/resources/views/includes/footer.blade.php
@@ -38,6 +38,22 @@
 
 
 <!--  Scripts-->
-<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="js/materialize.js"></script>
-<script src="js/init.js"></script>
+ <!--JS-->
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function(){
+      // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+      $('.modal').modal();
+
+      $('#textarea1').trigger('autoresize');
+
+      $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+      });
+
+      $('select').material_select();
+        
+    });
+  </script>
