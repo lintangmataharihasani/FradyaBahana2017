0566c3c07abef5196f3c6c73298d8d2db8d4c93d
hafizh19adnan <hafizh19adnan@gmail.com>
2017-09-07 17:32:46 +0700

    Fix 404

---
+++ backend/resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.default')
+
+@section('content')
+<div id="index-banner" class="parallax-container">
+   
+  <h1 class="header center white-text text-shadow">Fradya Bahana Innovation</h1>    
+</div>
+
+<section id="errors">
+  <div class="container">
+      <h4>Halaman Yang Anda Maksud tidak ditemukan</h4>
+      <a href="home">Back To Home</a>    
+  </div>
+</section>
+
+
+@stop
